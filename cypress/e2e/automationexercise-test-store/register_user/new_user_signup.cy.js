8b551458a6cc158f515068d5f050ca3cc3b10b0d
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-05-07 13:52:17 +0200

    Test: Signup new user email, name

--- cypress/e2e/automationexercise-test-store/register_user/new_user_signup.cy.js
+++ cypress/e2e/automationexercise-test-store/register_user/new_user_signup.cy.js
@@ -4,6 +4,12 @@
 
 describe("Test Signup new user email, name", () => {
   it("Should be able to submit a successful submission via signup form", () => {
+    const userName = "Alexandr";
+
     cy.navigateTo_Automationexercise_Loginpage();
+
+    cy.get('[data-qa="signup-name"]').type(userName);
+    cy.get('[data-qa="signup-email"]').type(`${Date.now()}@gmail.com`);
+    cy.get('[data-qa="signup-button"]').click({ force: true });
   });
 });
